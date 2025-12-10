17dbfdb73970bb904ed08dd1db14449edaf7347e
Vinh <danghuuvinh94@gmail.com>
2025-12-10 20:06:16 +1100

    init

---
+++ src/MinHeap.java
@@ -0,0 +1,102 @@
+public class MinHeap {
+	private int [] minHeap;
+	private int size;
+	
+	public MinHeap (int capacity) {
+		this.minHeap= new int [capacity];
+		this.size=0;
+	}
+	
+	public void heapifyUp(int index) {//nếu node có giá trị nhỏ hơn root cha thì đẩy node con lên vị trí 
+		while(index>0)
+		{
+			int parent=(index-1)/2;
+			if(minHeap[index]<minHeap[parent]) {
+				swap(index,parent);
+				index=parent;
+			} else {
+				break;
+			}
+		}
+	}
+	
+	public void insert (int value) {
+		minHeap[size]=value;
+		heapifyUp(size);
+		size++;
+	}
+	
+	public void swap(int a, int b) {
+		int temp=minHeap[a];
+		minHeap[a]=minHeap[b];
+		minHeap[b]=temp;
+	}
+	
+	public int removeMin() { //xóa root và gán root bằng node cuối 
+        int minValue = minHeap[0];
+
+        minHeap[0] = minHeap[size - 1];
+        size--;
+
+        heapifyDown(0);
+        return minValue;
+    }
+
+    public int getMin() {
+        return minHeap[0];
+    }
+
+
+     private void heapifyDown(int index) { // root có giá trị lớn hơn các node con thì đẩy giá trị root  
+         while (index < size) {
+             int left = 2 * index + 1;
+             int right = 2 * index + 2;
+
+             int smallest = index;
+
+             if (left < size && minHeap[left] < minHeap[smallest]) {
+                 smallest = left;
+             }
+             if (right < size && minHeap[right] < minHeap[smallest]) {
+                 smallest = right;
+             }
+
+             if (smallest != index) {
+                 swap(index, smallest);
+                 index = smallest;
+             } else {
+                 break;
+             }
+         }
+     }
+     
+     public static int findNthLargest(int[] arr, int n) {
+         MinHeap heap = new MinHeap(n);
+
+         for (int x : arr) {
+
+             if (heap.size < n) {
+                 heap.insert(x);
+             } else {
+                 if (x > heap.getMin()) {
+                     heap.removeMin();
+                     heap.insert(x);
+                 }
+             }
+         }
+
+         return heap.getMin(); 
+     }
+
+     
+     public static void main(String[] args) {
+         int[] arr = {4, 7, 2, 3, 9, 8, 1};
+
+         System.out.println(findNthLargest(arr, 1));
+         System.out.println(findNthLargest(arr, 2)); 
+         System.out.println(findNthLargest(arr, 3)); 
+         System.out.println(findNthLargest(arr, 7)); 
+     }
+}
+
+
